81eb1cf9252e6c901a2a68d4e104c6f77c4704d8
phi <phi.friday@gmail.com>
2024-03-10 15:58:19 +0900

    fix: output

--- src/action/setup.sh
+++ src/action/setup.sh
@@ -4,8 +4,10 @@ rye config --set-bool behavior.use-uv=$INPUT_USE_UV
 rye pin cpython@$INPUT_PYTHON_VERSION
 
 INSTALLED_RYE_VERSION=$(rye --version | awk '{print $2}' | head -n 1)
+INSTALLED_RYE_HOME=$(dirname $(dirname $(which rye)))
 PINNED_PYTHON_VERSION=$(cat .python-version)
 USE_UV=$(rye config --get behavior.use-uv)
 echo "rye-version=${INSTALLED_RYE_VERSION}"    >> $GITHUB_OUTPUT
+echo "rye-home=${INSTALLED_RYE_HOME}"          >> $GITHUB_OUTPUT
 echo "python-version=${PINNED_PYTHON_VERSION}" >> $GITHUB_OUTPUT
 echo "use-uv=${USE_UV}"                        >> $GITHUB_OUTPUT
